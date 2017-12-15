01c7d1ee505895f2ffc5c03515289fdc89e8ea17
Riley Flynn <riley@rileyflynn.me>
2017-12-15 15:33:14 -0330

    Added command line client, and added exchange-related methods and objects to the library

---
+++ cmd/coinbin/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"fmt"
+	"github.com/nint8835/coinbin"
+)
+
+func main() {
+
+	fromName := flag.String("from", "none", "The currency to convert from.")
+	toName := flag.String("to", "usd", "The currency to convert to.")
+	currencyAmount := flag.Float64("amount", 1.0, "The amount of the currency to convert.")
+
+	flag.Parse()
+
+	if *fromName == "none" {
+		fmt.Fprint(os.Stderr, "No from currency provided.")
+		os.Exit(1)
+	}
+
+
+	coin, err := coinbin.GetCoin(*fromName)
+
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error retrieving information for coin %s: %s", *fromName, err)
+		os.Exit(1)
+	}
+
+	if *toName == "usd" {
+		coinVal, err := coinbin.GetCoinValue(*fromName, *currencyAmount)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error while attempting to retrieve coin value: %s", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%f %s -> %f United States Dollars", *currencyAmount, coin.Name, coinVal.USDValue)
+	} else {
+		coinTwo, err := coinbin.GetCoin(*toName)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error retrieving information for coin %s: %s", *fromName, err)
+			os.Exit(1)
+		}
+		exchange, err := coinbin.GetCoinExchangeValue(*fromName, *toName, *currencyAmount)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error retrieving exchange information: %s", err)
+			os.Exit(1)
+		}
+		fmt.Printf("%f %s -> %f %s", *currencyAmount, coin.Name, exchange.Value, coinTwo.Name)
+	}
+
+
+
+}
--- coinbin.go
+++ coinbin.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"encoding/json"
+	"errors"
 )
 
 type Coin struct {
@@ -32,11 +33,32 @@ type CoinValueResponse struct {
 	CoinValue CoinValue `json:"coin"`
 }
 
+type CoinExchangeRate struct {
+	ExchangeRate float64 `json:"exchange_rate"`
+}
+
+type CoinExchangeRateResponse struct {
+	CoinExchangeRate CoinExchangeRate `json:"coin"`
+}
+
+type CoinExchangeValue struct {
+	ExchangeRate float64 `json:"exchange_rate"`
+	Value        float64 `json:"value"`
+	ResultCoin   string `json:"value.coin"`
+}
+
+type CoinExchangeValueResponse struct {
+	CoinExchangeValue CoinExchangeValue `json:"coin"`
+}
+
 func GetCoin(name string) (Coin, error) {
 	url := fmt.Sprintf("https://coinbin.org/%s", name)
 	response, err := http.Get(url)
 	if err != nil {
 		return Coin{}, err
+	}
+	if response.StatusCode == 500 {
+		return Coin{}, errors.New("specified coin does not exist")
 	}
 	defer response.Body.Close()
 
@@ -61,6 +83,9 @@ func GetCoinValue(name string, amount float64) (CoinValue, error) {
 	if err != nil {
 		return CoinValue{}, err
 	}
+	if response.StatusCode == 500 {
+		return CoinValue{}, errors.New("specified coin does not exist")
+	}
 	defer response.Body.Close()
 
 	data, err := ioutil.ReadAll(response.Body)
@@ -76,4 +101,56 @@ func GetCoinValue(name string, amount float64) (CoinValue, error) {
 	}
 
 	return coinValueResponse.CoinValue, err
+}
+
+func GetCoinExchangeRate(from string, to string) (CoinExchangeRate, error) {
+	url := fmt.Sprintf("https://coinbin.org/%s/to/%s", from, to)
+	response, err := http.Get(url)
+	if err != nil {
+		return CoinExchangeRate{}, err
+	}
+	if response.StatusCode == 500 {
+		return CoinExchangeRate{}, errors.New("specified coin does not exist")
+	}
+	defer response.Body.Close()
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return CoinExchangeRate{}, err
+	}
+
+	var coinExchangeRateResponse CoinExchangeRateResponse
+
+	err = json.Unmarshal(data, &coinExchangeRateResponse)
+	if err != nil {
+		return CoinExchangeRate{}, err
+	}
+
+	return coinExchangeRateResponse.CoinExchangeRate, err
+}
+
+func GetCoinExchangeValue(from string, to string, amount float64) (CoinExchangeValue, error) {
+	url := fmt.Sprintf("https://coinbin.org/%s/%f/to/%s", from, amount, to)
+	response, err := http.Get(url)
+	if err != nil {
+		return CoinExchangeValue{}, err
+	}
+	if response.StatusCode == 500 {
+		return CoinExchangeValue{}, errors.New("specified coin does not exist")
+	}
+	defer response.Body.Close()
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		return CoinExchangeValue{}, err
+	}
+
+	var coinExchangeValueResponse CoinExchangeValueResponse
+
+	err = json.Unmarshal(data, &coinExchangeValueResponse)
+	if err != nil {
+		return CoinExchangeValue{}, err
+	}
+
+	return coinExchangeValueResponse.CoinExchangeValue, err
 }
